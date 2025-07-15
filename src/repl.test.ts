c21c5c4dca4e9a950771f9a8a74087c61d1fc961
bdbrwr <bart@bdbrwr.nl>
2025-07-15 10:50:07 +0200

    Clean input and tests

---
+++ src/repl.test.ts
@@ -0,0 +1,34 @@
+import { cleanInput } from "./repl";
+import { describe, expect, test } from "vitest";
+
+describe.each([
+    {
+        input: "    ",
+        expected: [],
+    },
+    {
+        input: "  hello    ",
+        expected: ["hello"]
+    },
+    {
+        input: "  hello  world  ",
+        expected: ["hello", "world"],
+    },
+    {
+        input: "  HeLlO  World  ",
+        expected: ["hello", "world"],
+    },
+    {
+        input: "Charmander Bulbasaur PIKACHU",
+        expected: ["charmander", "bulbasaur", "pikachu"],
+    },
+
+])("cleanInput($input)", ({ input, expected }) => {
+    test(`Expected: ${expected}`, () => {
+        const actual = cleanInput(input);
+        expect(actual).toHaveLength(expected.length);
+        for (const i in expected) {
+            expect(actual[i]).toBe(expected[i]);
+        }
+    });
+});
